c2bddf5505970d07b2dab670ac436120479587d7
Zering-Max <maxlev.oclock@gmail.com>
2020-06-19 15:38:18 +0200

    connect delete request socket OK

--- front/src/middlewares/socket.js
+++ front/src/middlewares/socket.js
@@ -1,4 +1,4 @@
-import { WS_CONNECT, SAVE_POST, addPost } from '../actions';
+import { WS_CONNECT, SAVE_POST, addPost, removePost, SAVE_DELETE_POST } from '../actions';
 import axios from 'axios';
 
 let socketCanal;
@@ -13,10 +13,25 @@ export const socket = (store) => (next) => (action) => {
       socketCanal.on('receive_post', (post) => {
       store.dispatch(addPost(post));
       });
+
+      socketCanal.on('remove_post', (id) => {
+        console.log(id);
+        store.dispatch(removePost(id));
+      });
+
+     
+
+    
       break;
     case SAVE_POST:
-      console.log("SAVE_POST")
       socketCanal.emit('add_post', action.post);
+      break;
+
+    case SAVE_DELETE_POST:
+      console.log('save_delete_post');
+      console.log('action.id' + action.id_post)
+      socketCanal.emit('delete_post', action.id_post);
+      break;
     default:
       next(action);
   }
