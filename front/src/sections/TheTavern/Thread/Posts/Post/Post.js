903f7da933d55b4bd726fd56ea42b8b1a7869fa3
Zering-Max <maxlev.oclock@gmail.com>
2020-06-19 18:26:20 +0200

    csss thread page in progress

--- front/src/sections/TheTavern/Thread/Posts/Post/Post.js
+++ front/src/sections/TheTavern/Thread/Posts/Post/Post.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './Post.module.scss';
+import getDateWithCreatedAt from '../../../../../util/getDateWithCreatedAt'
 
 
-const Post = ({ title, content, author, image, color, deletePost, loggedUser, id  }) => {
+const Post = ({ title, content, author, image, color, deletePost, loggedUser, id, createdAt }) => {
+  console.log(getDateWithCreatedAt(createdAt));
   return (
     
-    <div>
-      <div>{author.username}</div>
-      <p>{content}</p>
-  { author.username === loggedUser && <button type="button" onClick={() => deletePost(id)} > Supprimmer  </button> }
-  { author.username !== loggedUser && <button type="button" > Ajouter  </button> }
+    <div className={styles.Post}>
+      { author.username === loggedUser ? <div className={styles.AuthorPost_gold}>{author.username}</div> : <div className={styles.AuthorPost}>{author.username}</div>}
+      <p className={styles.ContentPost}>{content}</p>
+      <div className={styles.PostFooter}>
+          { author.username === loggedUser && <button type="button" onClick={() => deletePost(id)} > Supprimer  </button> }
+          { author.username !== loggedUser && <button type="button" > Ajouter  </button> }
+
+      </div>
     </div>
   );
 }
