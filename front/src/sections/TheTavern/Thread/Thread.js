89d68b59b1a295dd031005029ada234bfec58387
TomasGortari <gortarithomas@gmail.com>
2020-06-23 09:15:13 +0200

    advance the css of thread page

--- front/src/sections/TheTavern/Thread/Thread.js
+++ front/src/sections/TheTavern/Thread/Thread.js
@@ -1,22 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Thread.module.scss';
 
 import cross from '../../../assets/images/logocroix.svg';
 import Posts from '../../../containers/TheTavern/Thread/Posts';
 import PostForm from '../../../containers/TheTavern/Thread/PostForm';
+import SendPostMobile from './SendPostMobile/SendPostMobile'
+import Invitation from './Invitation/Invitation';
+import Search from './Search/Search'
 
 const Thread = () => {
+
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className={styles.Thread}>
       
       <PostForm />
-      <Posts />
-      <div className={styles.Drawer}>
+      { !isOpen && <Posts /> }
+     <div className={ !isOpen ? styles.Drawer : styles.Drawer_open}>
         <img className={styles.SelfAvatar} src={cross} alt=""/>
-        <div className={styles.Triangle}></div>
+        <div onClick={() => setIsOpen(!isOpen)} className={ !isOpen ? styles.Triangle : styles.Triangle_down}></div>
       </div>
+      { isOpen && <div>
+                    <SendPostMobile /> 
+                    <div className={styles.Invitation}>
+                      <Invitation name='EdgeAbonnement' avatar={cross} />
+                      <Invitation name='LaServeuse' avatar={cross} />
+                      <Invitation name='TuVeuxEtreMonAMIE' avatar={cross} />
+                    </div>
+                    <Search />
+                  </div>}
     </div>
   );
 };
+
 
 export default Thread;
