413ae580cd1004cce8e94554f73fbe16ddb43db3
ZayenJS <davidnogueira.dev.pro@gmail.com>
2020-07-01 17:04:22 +0200

    fixed gamemaster issue in tavern

--- front/src/sections/TheTavern/Tavern/ConnectedContactsList/ConnectedContactsList.js
+++ front/src/sections/TheTavern/Tavern/ConnectedContactsList/ConnectedContactsList.js
@@ -3,22 +3,38 @@ import PropTypes from 'prop-types';
 
 import styles from './ConnectedContactsList.module.scss';
 
-const ConnectedContactsList = ({connectedContacts}) => {
+const ConnectedContactsList = ({ connectedContacts, user }) => {
   return (
     <div className={styles.ConnectedContactsList}>
       <ul className={styles.ConnectedContactsList__List}>
-        {connectedContacts && connectedContacts.map((user) => ( user.isInitiator ? null :
+        {!user.isGamemaster && (
           <li className={styles.ConnectedContactsList__List__Item} key={user.id}>
-            <img title={user.username} className={styles.ConnectedContactsList__List__Avatar} src={user.avatar} alt={`Avatar de ${user.username}`}/>
+            <img
+              title={user.username}
+              className={styles.ConnectedContactsList__List__Avatar}
+              src={user.avatar}
+              alt={`Avatar de ${user.username}`}
+            />
           </li>
-        ))}
+        )}
+
+        {connectedContacts.map((contact) =>
+          (contact.isGamemaster && contact.id === user.id) || contact.isGamemaster ? null : (
+            <li className={styles.ConnectedContactsList__List__Item} key={contact.id}>
+              <img
+                title={contact.username}
+                className={styles.ConnectedContactsList__List__Avatar}
+                src={contact.avatar}
+                alt={`Avatar de ${contact.username}`}
+              />
+            </li>
+          ),
+        )}
       </ul>
     </div>
   );
 };
 
-ConnectedContactsList.propTypes = {
-  
-};
+ConnectedContactsList.propTypes = {};
 
 export default ConnectedContactsList;
