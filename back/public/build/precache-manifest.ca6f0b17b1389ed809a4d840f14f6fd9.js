c3e77f10aed0baea0ac762d2db79b9f9092c6547
Fabien-Pinna <fabien.pinna@hotmail.fr>
2020-09-05 17:16:11 +0200

    create a builder for the front

---
+++ back/public/build/precache-manifest.ca6f0b17b1389ed809a4d840f14f6fd9.js
@@ -0,0 +1,206 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d1c09f8da9ef2272647e0704b600360",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dddb2f118a7b857f1426",
+    "url": "/static/css/main.76b6b080.chunk.css"
+  },
+  {
+    "revision": "a9706cd82d7130a58c3d",
+    "url": "/static/js/2.d1394080.chunk.js"
+  },
+  {
+    "revision": "603816eec26c1aff4a38bef8d08d40a6",
+    "url": "/static/js/2.d1394080.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dddb2f118a7b857f1426",
+    "url": "/static/js/main.b4cbba3b.chunk.js"
+  },
+  {
+    "revision": "a9826cacf95ce8ad1ce1",
+    "url": "/static/js/runtime-main.6e4d6195.js"
+  },
+  {
+    "revision": "c4b5987e83e5cc585f68dddaf781c883",
+    "url": "/static/media/404.c4b5987e.png"
+  },
+  {
+    "revision": "6c6c318fad15c921b61fb3bfdfc4ed9d",
+    "url": "/static/media/Avatardefault.6c6c318f.png"
+  },
+  {
+    "revision": "ee2cdef198faccc1052d5bf99e1e9a86",
+    "url": "/static/media/Avatardefault.ee2cdef1.png"
+  },
+  {
+    "revision": "cce72e4c2ba4133017dfa0c037cab3ac",
+    "url": "/static/media/Dwarf_male_bg.cce72e4c.png"
+  },
+  {
+    "revision": "13241e3cb288089ac8e037653c169865",
+    "url": "/static/media/Ratmen_male_bg.13241e3c.png"
+  },
+  {
+    "revision": "73bf049a3a3974bb47637aab04e63df1",
+    "url": "/static/media/addplayer.73bf049a.svg"
+  },
+  {
+    "revision": "c698a3cacf3e263eac990f0d9089a754",
+    "url": "/static/media/background10.c698a3ca.png"
+  },
+  {
+    "revision": "a1bc9cd0cf7328ca329c59e5e61bc2e3",
+    "url": "/static/media/background4.a1bc9cd0.jpg"
+  },
+  {
+    "revision": "bf7bd470757eaad0042b852eebe134c7",
+    "url": "/static/media/backgroundTavernAsk.bf7bd470.png"
+  },
+  {
+    "revision": "8cedcc6ce2de2fbff62de2b8acd239a7",
+    "url": "/static/media/backgroundlight.8cedcc6c.jpg"
+  },
+  {
+    "revision": "3dfe07244e0ef8b7523d1200f3c2581c",
+    "url": "/static/media/backgroundvideo.3dfe0724.mp4"
+  },
+  {
+    "revision": "9d1620e530857696b3e9650d6c067717",
+    "url": "/static/media/boutoncroix.9d1620e5.svg"
+  },
+  {
+    "revision": "815ee7edbc6fb4f2bacc5afa8aa2581f",
+    "url": "/static/media/d10.815ee7ed.svg"
+  },
+  {
+    "revision": "7f1b8d4006ecf97d75358c0506a7f356",
+    "url": "/static/media/d12.7f1b8d40.svg"
+  },
+  {
+    "revision": "b54abe067190fd4cc3a499f5a4556c30",
+    "url": "/static/media/d20.b54abe06.svg"
+  },
+  {
+    "revision": "894ea704ec2a1cde20c14adbda34a7d0",
+    "url": "/static/media/d4.894ea704.svg"
+  },
+  {
+    "revision": "decfdae0e45aa28103ac3800d328f589",
+    "url": "/static/media/d6.decfdae0.svg"
+  },
+  {
+    "revision": "a6119591b7ea1b1f12220f4cb807b0c7",
+    "url": "/static/media/d8.a6119591.svg"
+  },
+  {
+    "revision": "96c14055961dcbf564e15238e3ca21bf",
+    "url": "/static/media/door.96c14055.svg"
+  },
+  {
+    "revision": "ced6a0750fb81e9753e9cdaa52403d4c",
+    "url": "/static/media/dwarf_female_bg.ced6a075.png"
+  },
+  {
+    "revision": "7ab329a61989c58d1086264c95910e95",
+    "url": "/static/media/elf_female_bg.7ab329a6.png"
+  },
+  {
+    "revision": "ed645a34b0a99e4fbbb84715a42b1d85",
+    "url": "/static/media/elf_male_bg.ed645a34.png"
+  },
+  {
+    "revision": "daa89045e9bb1db5b6cc9d065e581a7c",
+    "url": "/static/media/feather.daa89045.svg"
+  },
+  {
+    "revision": "cb62af30d7509a6f0c26acc94922b933",
+    "url": "/static/media/fire.cb62af30.svg"
+  },
+  {
+    "revision": "086a630800f624464695d7d2e544a9c0",
+    "url": "/static/media/gnome_female_bg.086a6308.png"
+  },
+  {
+    "revision": "101d3e48d6c872a7e5571719aa62457b",
+    "url": "/static/media/gnome_male_bg.101d3e48.png"
+  },
+  {
+    "revision": "02951be872c5077e0b34595484cfb4f5",
+    "url": "/static/media/goblin.02951be8.gif"
+  },
+  {
+    "revision": "27dd6986cb48d1ccb4efce80f25d570a",
+    "url": "/static/media/halfelf_female_bg.27dd6986.png"
+  },
+  {
+    "revision": "9675b78a5657d41226b3af1ee4d1d412",
+    "url": "/static/media/halfelf_male_bg.9675b78a.png"
+  },
+  {
+    "revision": "488a94ac33fbd62e084acea53c31219e",
+    "url": "/static/media/human_female_bg.488a94ac.png"
+  },
+  {
+    "revision": "ae49f34dbe4988c71850d0385975f2a3",
+    "url": "/static/media/human_male_bg.ae49f34d.png"
+  },
+  {
+    "revision": "b520410929b3eefb5a8bff49e4f12266",
+    "url": "/static/media/img-noise.b5204109.png"
+  },
+  {
+    "revision": "27482540333bbcd4e0158bf29f827ef6",
+    "url": "/static/media/lizardmen_female_bg.27482540.png"
+  },
+  {
+    "revision": "cebd58c674023b2d4bec97badb4ed4d4",
+    "url": "/static/media/lizardmen_male_bg.cebd58c6.png"
+  },
+  {
+    "revision": "dc9a0cbdc019a1f299df96446600554e",
+    "url": "/static/media/logo1.dc9a0cbd.svg"
+  },
+  {
+    "revision": "5e81e88f88285f0c5a119c3913d40b49",
+    "url": "/static/media/logocroix.5e81e88f.svg"
+  },
+  {
+    "revision": "7788de45765485fdaec38545b6122dfa",
+    "url": "/static/media/orc_female_bg.7788de45.png"
+  },
+  {
+    "revision": "a2b0799e4d2944d267c24d876eecf54c",
+    "url": "/static/media/orc_male_bg.a2b0799e.png"
+  },
+  {
+    "revision": "15951062ad54904d76067f9770355fcf",
+    "url": "/static/media/ratmen_female_bg.15951062.png"
+  },
+  {
+    "revision": "6d0f02c4db8f0259cd7af8b952fbd0c9",
+    "url": "/static/media/scroll.6d0f02c4.svg"
+  },
+  {
+    "revision": "daa42c3da47ddd366b0a124ab0a01504",
+    "url": "/static/media/shield.daa42c3d.svg"
+  },
+  {
+    "revision": "02fb4003a1e411d8aa8eb253203c719f",
+    "url": "/static/media/skull.02fb4003.svg"
+  },
+  {
+    "revision": "6857bbe42e6e2f29b4b2e821159868d1",
+    "url": "/static/media/team.6857bbe4.svg"
+  },
+  {
+    "revision": "f540b25a2de036fee3c49599840bc0aa",
+    "url": "/static/media/turn-off.f540b25a.svg"
+  },
+  {
+    "revision": "5972d9437612b97901814ee0b30752cd",
+    "url": "/static/media/wood3.5972d943.jpg"
+  }
+]);
